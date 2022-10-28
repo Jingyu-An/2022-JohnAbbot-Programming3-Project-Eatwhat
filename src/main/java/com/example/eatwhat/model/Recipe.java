f22fc9e06c3b3c1dc9102e0ecb61b3c11e2a0cba
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-10-28 14:27:17 -0400

    - add lombok dependency - fix model sources , remove getter/setter (using lombok) - remove Role.java

--- src/main/java/com/example/eatwhat/model/Recipe.java
+++ src/main/java/com/example/eatwhat/model/Recipe.java
@@ -1,10 +1,13 @@
 package com.example.eatwhat.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 import java.util.Date;
 
 @Entity
 @Table(name = "eatwhat_recipe")
+@Data
 public class Recipe {
   
   @Id
@@ -14,44 +17,5 @@ public class Recipe {
   private String recipeDescription;
   private Date recipeDate;
   private long point;
-  
-  public long getRecipeId() {
-    return recipeId;
-  }
-  
-  public void setRecipeId(long recipeId) {
-    this.recipeId = recipeId;
-  }
-  
-  public String getRecipeTitle() {
-    return recipeTitle;
-  }
-  
-  public void setRecipeTitle(String recipeTitle) {
-    this.recipeTitle = recipeTitle;
-  }
-  
-  public String getRecipeDescription() {
-    return recipeDescription;
-  }
-  
-  public void setRecipeDescription(String recipeDescription) {
-    this.recipeDescription = recipeDescription;
-  }
-  
-  public Date getRecipeDate() {
-    return recipeDate;
-  }
-  
-  public void setRecipeDate(Date recipeDate) {
-    this.recipeDate = recipeDate;
-  }
-  
-  public long getPoint() {
-    return point;
-  }
-  
-  public void setPoint(long point) {
-    this.point = point;
-  }
+
 }
--- src/main/java/com/example/eatwhat/model/User.java
+++ src/main/java/com/example/eatwhat/model/User.java
@@ -1,9 +1,12 @@
 package com.example.eatwhat.model;
 
+import lombok.Data;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "eatwhat_user")
+@Data
 public class User {
   
   @Id
@@ -13,44 +16,5 @@ public class User {
   private String userEmail;
   private String userPassword;
   private long userPoint;
-  
-  public long getUserId() {
-    return userId;
-  }
-  
-  public void setUserId(long userId) {
-    this.userId = userId;
-  }
-  
-  public String getUsername() {
-    return username;
-  }
-  
-  public void setUsername(String username) {
-    this.username = username;
-  }
-  
-  public String getUserEmail() {
-    return userEmail;
-  }
-  
-  public void setUserEmail(String userEmail) {
-    this.userEmail = userEmail;
-  }
-  
-  public String getUserPassword() {
-    return userPassword;
-  }
-  
-  public void setUserPassword(String userPassword) {
-    this.userPassword = userPassword;
-  }
-  
-  public long getUserPoint() {
-    return userPoint;
-  }
-  
-  public void setUserPoint(long userPoint) {
-    this.userPoint = userPoint;
-  }
+
 }
