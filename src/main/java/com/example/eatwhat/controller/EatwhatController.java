89e97260c96c79aaf1697f22e25e6c0cf0f62270
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-10-29 18:42:22 -0400

    Add security option. Authentication  - user & admin(temporary)

--- src/main/java/com/example/eatwhat/controller/EatwhatController.java
+++ src/main/java/com/example/eatwhat/controller/EatwhatController.java
@@ -3,12 +3,12 @@ package com.example.eatwhat.controller;
 import com.example.eatwhat.model.Recipe;
 import com.example.eatwhat.service.RecipeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.view.RedirectView;
 
@@ -17,36 +17,70 @@ import java.util.List;
 
 @Controller
 public class EatwhatController {
-  
-  @Autowired
-  private RecipeService recipeService;
-  
-  @RequestMapping("/")
-  public String homePage() {
-    return "index";
-  }
-  
-  @RequestMapping("/register_recipe")
-  public String showRecipe(Model model) {
-    Recipe recipe = new Recipe();
-    model.addAttribute("recipe", recipe);
-    
-    return "register_recipe";
-  }
-  
-  @RequestMapping(value = "/recipe/save", method = RequestMethod.POST)
-  public RedirectView saveRecipe(@ModelAttribute("recipe") Recipe recipe,
-                                 @RequestParam("image")MultipartFile multipartFile) throws IOException {
-    recipeService.save(recipe, multipartFile);
-  
-    return new RedirectView("/recipe", true);
-  }
-  
-  @RequestMapping("/recipe")
-  public String viewRecipe(Model model) {
-    List<Recipe> listRecipes = recipeService.listAll();
-    model.addAttribute("listRecipes", listRecipes);
-  
-    return "recipe";
-  }
+
+    @Autowired
+    private RecipeService recipeService;
+
+    @RequestMapping("/")
+    public String homePage() {
+        return "index";
+    }
+
+    @RequestMapping("/register_recipe")
+    public String showRecipe(Model model) {
+        Recipe recipe = new Recipe();
+        model.addAttribute("recipe", recipe);
+
+        return "register_recipe";
+    }
+
+    @RequestMapping(value = "/recipe/save", method = RequestMethod.POST)
+    public RedirectView saveRecipe(@ModelAttribute("recipe") Recipe recipe,
+                                   @RequestParam("image") MultipartFile multipartFile) throws IOException {
+        recipeService.save(recipe, multipartFile);
+
+        return new RedirectView("/recipe", true);
+    }
+
+    @RequestMapping("/recipe")
+    public String viewRecipe(Model model) {
+        List<Recipe> listRecipes = recipeService.listAll();
+        model.addAttribute("listRecipes", listRecipes);
+
+        return "recipe";
+    }
+
+    @GetMapping("/auth")
+    @ResponseBody
+    public Authentication auth() {
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "loginForm";
+    }
+
+    @GetMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "loginForm";
+    }
+
+    @GetMapping("/access-denied")
+    public String accessDenied() {
+        return "AccessDenied";
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_USER')")
+    @GetMapping("/user-page")
+    public String userPage() {
+        return "UserPage";
+    }
+
+    @PreAuthorize("hasAnyAuthority('ROLE_ADMIN')")
+    @GetMapping("/admin-page")
+    public String adminPage() {
+        return "AdminPage";
+    }
 }
