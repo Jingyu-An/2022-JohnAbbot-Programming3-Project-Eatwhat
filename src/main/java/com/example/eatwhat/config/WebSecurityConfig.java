9b9eea231a513d2d3a5847e10cbb8f6a90e1d713
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-11-03 14:51:50 -0400

    Login error fix

--- src/main/java/com/example/eatwhat/config/WebSecurityConfig.java
+++ src/main/java/com/example/eatwhat/config/WebSecurityConfig.java
@@ -17,50 +17,52 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-  
-  @Bean
-  public UserDetailsService userDetailsService() {
-    return new UserService();
-  }
-  
-  @Bean
-  public BCryptPasswordEncoder passwordEncoder() {
-    return new BCryptPasswordEncoder();
-  }
-  
-  @Bean
-  public DaoAuthenticationProvider authenticationProvider() {
-    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-    authProvider.setUserDetailsService(userDetailsService());
-    authProvider.setPasswordEncoder(passwordEncoder());
-    
-    return authProvider;
-  }
-  
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.authenticationProvider(authenticationProvider());
-  }
-  
-  @Override
-  protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        .antMatchers("/user/index").hasRole("USER")
-        .antMatchers("/manager/index").hasRole("ADMIN")
-        .anyRequest().permitAll()
-        .and()
-        .formLogin()
-        .loginPage("/login")
-        .usernameParameter("username")
-        .defaultSuccessUrl("/")
-        .permitAll()
-        .and()
-        .logout().logoutSuccessUrl("/").permitAll();
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new UserService();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/user/index").hasRole("USER")
+                .antMatchers("/manager/index").hasRole("ADMIN")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+//                .loginProcessingUrl("/loginprocess")
+                .usernameParameter("username")
+                .defaultSuccessUrl("/", false)
+                .failureUrl("/login-error")
+                .permitAll()
+                .and()
+                .logout().logoutSuccessUrl("/").permitAll();
 //      http
 //              .authorizeRequests(request->
 //                      request.anyRequest().permitAll()
 //              );
-  }
+    }
 
     @Override
     public void configure(WebSecurity web) throws Exception {
