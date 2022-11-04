8429f77bb6c1786b07e7428e97e92f8624dca3af
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-11-04 16:17:15 -0400

    user and recipe connect. when user register the recipe, recipe has user information

--- src/main/java/com/example/eatwhat/controller/RecipeController.java
+++ src/main/java/com/example/eatwhat/controller/RecipeController.java
@@ -2,10 +2,13 @@ package com.example.eatwhat.controller;
 
 import com.example.eatwhat.model.Recipe;
 import com.example.eatwhat.model.RecipeCategory;
+import com.example.eatwhat.model.User;
 import com.example.eatwhat.service.RecipeCategoryService;
 import com.example.eatwhat.service.RecipeService;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -19,7 +22,7 @@ import java.util.List;
 
 @Controller
 @NoArgsConstructor
-@RequestMapping(value="/recipe")
+@RequestMapping(value = "/recipe")
 
 public class RecipeController {
 
@@ -33,62 +36,64 @@ public class RecipeController {
 //        return "sth" // TODO check it out
 //    }
 
-        public void initList (Model model){
-            List<Recipe> listRecipes = recipeService.listAll(); // Need to change with inner join
-            model.addAttribute("listRecipes", listRecipes);
-        }
+    public void initList(Model model) {
+        List<Recipe> listRecipes = recipeService.listAll(); // Need to change with inner join
+        model.addAttribute("listRecipes", listRecipes);
+    }
 
-        @GetMapping({"", "/"})
-        public String index (Model model){
+    @GetMapping({"", "/"})
+    public String index(Model model) {
 
-            initList(model);
+        initList(model);
+        return "/recipe/index";
+    }
+
+    @GetMapping("/signup")
+    public String signUp(Model model) {
+        Recipe recipe = new Recipe();
+        model.addAttribute("recipe", recipe);
+        System.out.println("This is signup method");
+        return "/recipe/newRecipe";
+    }
+
+    @RequestMapping(value = "/register/save", method = RequestMethod.POST)
+    public String save(@Valid @ModelAttribute("recipe") Recipe recipe, BindingResult bindingResult,
+                       @RequestParam("image") MultipartFile multipartFile, Model model) throws IOException {
+
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("recipe", recipe);
             return "/recipe/index";
         }
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        User user = (User) authentication.getPrincipal();
+        recipe.setUser(user);// insert user information
+        recipeService.save(recipe, multipartFile);
 
-        @GetMapping("/signup")
-        public String signUp (Model model){
-            Recipe recipe = new Recipe();
-            model.addAttribute("recipe", recipe);
-            System.out.println("This is signup method");
-            return "/recipe/newRecipe";
-        }
-
-        @RequestMapping(value = "/register/save", method = RequestMethod.POST)
-        public String save (@Valid @ModelAttribute("recipe") Recipe recipe, BindingResult bindingResult,
-                            @RequestParam("image") MultipartFile multipartFile, Model model) throws IOException {
-
-            if (bindingResult.hasErrors()) {
-                model.addAttribute("recipe", recipe);
-                return "/recipe/index";
-            }
-
-            recipeService.save(recipe, multipartFile);
-
-            return index(model);
-        }
-
-        @RequestMapping("/edit/{id}")
-        public String edit ( @PathVariable(name = "id") long id, Model model){
-            Recipe recipe = recipeService.get(id);
-            model.addAttribute("recipe", recipe);
-
-            return "/recipe/edit";
-        }
-
-        @RequestMapping("/delete/{id}")
-        public String delete ( @PathVariable(name = "id") long id, Model model){
-            recipeService.delete(id);
-
-            return index(model);
-        }
-
-
-        @GetMapping("/showCreateRecipe")
-        public String showCreateRecipe (Model model){
-            List<RecipeCategory> recipeCategories = recipeCategoryService.getAll();
-            model.addAttribute("recipeCategories", recipeCategories);
-            return "/recipe/newRecipe";
-        }
-
+        return index(model);
     }
+
+    @RequestMapping("/edit/{id}")
+    public String edit(@PathVariable(name = "id") long id, Model model) {
+        Recipe recipe = recipeService.get(id);
+        model.addAttribute("recipe", recipe);
+
+        return "/recipe/edit";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delete(@PathVariable(name = "id") long id, Model model) {
+        recipeService.delete(id);
+
+        return index(model);
+    }
+
+
+    @GetMapping("/showCreateRecipe")
+    public String showCreateRecipe(Model model) {
+        List<RecipeCategory> recipeCategories = recipeCategoryService.getAll();
+        model.addAttribute("recipeCategories", recipeCategories);
+        return "/recipe/newRecipe";
+    }
+
+}
 
