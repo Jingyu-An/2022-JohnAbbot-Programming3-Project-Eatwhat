3f2803dee510c01675dda0df54afc8a609f18686
Lucie-HJ-ROH <lucie.hj.roh@gmail.com>
2022-11-06 01:32:10 -0400

    Revert "user list"
    This reverts commit 177578905f1ea7e6f53315e3ee99cf8a732a3bf5.

--- src/main/java/com/example/eatwhat/dao/RecipeRepository.java
+++ src/main/java/com/example/eatwhat/dao/RecipeRepository.java
@@ -1,15 +1,15 @@
 package com.example.eatwhat.dao;
 
 import com.example.eatwhat.model.Recipe;
-import com.example.eatwhat.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface RecipeRepository extends JpaRepository<Recipe, Long> {
 
-//    @Query("SELECT r FROM Recipe r WHERE r.user = ?user")
-    List<Recipe> findAllByUser(User user);
+    @Query("select r from Recipe r, User u where r.id =u.id")
+    List<Recipe> findAllByRecipeIn(long userId);
+
+
 }
--- src/main/java/com/example/eatwhat/service/RecipeService.java
+++ src/main/java/com/example/eatwhat/service/RecipeService.java
@@ -23,7 +23,7 @@ public class RecipeService {
   public List<Recipe> listAll() {
     return repo.findAll();
   }
-//  public List<Recipe> listAllByUser(Long id) {return repo.findAllByRecipeIn(id);}
+  public List<Recipe> listAllByUser(Long userId) {return repo.findAllByRecipeIn(userId);}
   
   public void save(Recipe recipe)  {
     repo.save(recipe);
